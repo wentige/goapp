7d5aff90c50505be147a27b1fa0b3627d20da35a
Hanson Li <lihsca@gmail.com>
2019-10-25 17:06:48 -0400

    pkg/utils/hashing+passhash+random

---
+++ pkg/utils/random.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"crypto/rand"
+)
+
+func RandomString(n int) string {
+	//const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const alphanum = "2345789ABCDEFHJKMNPQRSTUVWXYZ"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+	}
+	return string(bytes)
+}
