27a4fa2a0819b12e53a2f3ddbe7823a1d44014cc
Gopher L <zhuyf2000@gmail.com>
2019-10-28 15:44:13 -0400

    supplier/asi/PriceAvail

--- supplier/asi/PriceAvail.go
+++ supplier/asi/PriceAvail.go
@@ -1,2 +1,44 @@
 package asi
 
+import (
+	"encoding/xml"
+)
+
+type Branch struct {
+	Code string `xml:"Code,attr"`
+	Name string `xml:"Name,attr"`
+	Qty  string `xml:",chardata"`
+}
+
+type BranchQty struct {
+	Branches []Branch `xml:"Branch"`
+}
+
+type Item struct {
+	SKU         string    `xml:"SKU,attr"`
+	ItemId      string    `xml:"ItemId"`
+	Description string    `xml:"Description"`
+	Vendor      string    `xml:"Vendor"`
+	Category    string    `xml:"Category"`
+	SubCategory string    `xml:"SubCategory"`
+	UPC         string    `xml:"UPC"`
+	Price       string    `xml:"Price"`
+	Rebate      string    `xml:"Rebate"`
+	Term        string    `xml:"Term"`
+	Weight      string    `xml:"Weight"`
+	Status      string    `xml:"Status"`
+	Qty         BranchQty `xml:"Qty"`
+}
+
+type Error struct {
+	Code    string `xml:"code"`
+	Message string `xml:"message"`
+}
+
+type ASIInventory struct {
+	XMLName  xml.Name `xml:"ASIInventory"`
+	DateTime string   `xml:"request,attr"`
+	Excute   string   `xml:"excute,attr"`
+	Items    []Item   `xml:"Inventory"`
+	Error    Error    `xml:"error"`
+}
--- supplier/asi/PriceAvail_test.go
+++ supplier/asi/PriceAvail_test.go
@@ -1,6 +1,7 @@
 package asi
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"testing"
@@ -17,10 +18,47 @@ func Test_Request(t *testing.T) {
 
 func Test_Response(t *testing.T) {
 	assert := assert.New(t)
-	assert.Nil(nil)
+
+	filename := "fixtures/PriceAvail-Response-1.xml"
+	data := getXML(filename)
+
+	var x ASIInventory
+
+	err := xml.Unmarshal(data, &x)
+	assert.Nil(err)
+
+	assert.Equal(x.Error.Code, "")
+
+	assert.Equal(len(x.Items), 1)
+	assert.Equal(x.Items[0].SKU, "209435")
+	assert.Equal(x.Items[0].ItemId, "TS-431XEU-2G-US")
+	assert.Equal(x.Items[0].UPC, "885022014033")
+	assert.Equal(x.Items[0].Status, "ACTIVE")
+
+	assert.Equal(len(x.Items[0].Qty.Branches), 2)
+
+	assert.Equal(x.Items[0].Qty.Branches[0].Name, "Toronto")
+	assert.Equal(x.Items[0].Qty.Branches[0].Code, "2216")
+	assert.Equal(x.Items[0].Qty.Branches[0].Qty, "2")
+
+	assert.Equal(x.Items[0].Qty.Branches[1].Name, "Vancouver")
+	assert.Equal(x.Items[0].Qty.Branches[1].Code, "3116")
+	assert.Equal(x.Items[0].Qty.Branches[1].Qty, "1")
 }
 
 func Test_Errors(t *testing.T) {
+	assert := assert.New(t)
+
+	filename := "fixtures/PriceAvail-Response-Error-1.xml"
+	data := getXML(filename)
+
+	var x ASIInventory
+
+	err := xml.Unmarshal(data, &x)
+	assert.Nil(err)
+
+	assert.Equal(x.Error.Code, "101")
+	assert.Equal(x.Error.Message, "No inventory found")
 }
 
 func getXML(filename string) []byte {
