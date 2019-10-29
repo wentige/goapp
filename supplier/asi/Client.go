721e10c733fcc1309621ad342188d2f05abf6884
Gopher L <zhuyf2000@gmail.com>
2019-10-29 09:13:43 -0400

    supplier/asi/Client

--- supplier/asi/Client.go
+++ supplier/asi/Client.go
@@ -1,7 +1,23 @@
 package asi
 
+import "log"
+
 type Config struct {
 }
 
 type Client struct {
+	Username string
+	Password string
+	//Logger
+}
+
+func (c *Client) LogError(err error) {
+	log.Println(err)
+}
+
+func (c *Client) LogData(sku, url, req, res string) {
+	log.Println(sku)
+	log.Println(url)
+	log.Println(req)
+	log.Println(res)
 }
--- supplier/asi/PriceAvail.go
+++ supplier/asi/PriceAvail.go
@@ -2,5 +2,65 @@ package asi
 
 import (
 	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"myapp/supplier/model"
+	"net/http"
+	"time"
 )
 
+const PA_URL = "https://www.asipartner.com/partneraccess/xml/price.asp"
+
+func (c *Client) GetPriceAvail(skus []string) *model.PriceAvailResult {
+	cid := c.Username
+	cert := c.Password
+	sku := skus[0] // one SKU per request
+
+	// build URL
+	param := fmt.Sprintf("?CID=%s&CERT=%s&SKU=%s", cid, cert, sku)
+	url := PA_URL + param
+
+	// new http request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// http header
+	req.Header.Add("Content-Type", "application/xml; charset=utf-8")
+
+	// http client
+	client := &http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	// do request
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		c.LogError(err)
+	}
+
+	// parse response
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		c.LogError(err)
+	}
+
+	var x ASIInventory
+	err = xml.Unmarshal(response, &x)
+	if err != nil {
+		c.LogError(err)
+	}
+
+	// logger
+	c.LogData(sku, PA_URL, param, string(response))
+
+	return ToPriceAvailResult(&x)
+}
+
+func ToPriceAvailResult(x *ASIInventory) *model.PriceAvailResult {
+	r := &model.PriceAvailResult{}
+	return r
+}
