e9ce504c223b331334de112a34a035d16de538db
Gopher L <zhuyf2000@gmail.com>
2019-10-29 15:58:55 -0400

    supplier/techdata/PriceAvail

--- supplier/techdata/PriceAvail.go
+++ supplier/techdata/PriceAvail.go
@@ -12,27 +12,56 @@ import (
 
 const PA_URL = "https://tdxml.techdata.com/xmlservlet"
 
-func (c *Client) GetPriceAvail(skus []string) *model.PriceAvailResult {
-	var in XMLPriceAvailSubmit
+type PriceAvail struct {
+	client *Client
+}
 
-	in.Header.Username = c.Username
-	in.Header.Password = c.Password
-	in.Header.Version = "1.4"
+func (self PriceAvail) Query(skus []string) *model.PriceAvailResult {
+	request := self.BuildRequest(skus)
+
+	response, err := self.SendRequest(PA_URL, request)
+	if err != nil {
+		self.client.LogError(err)
+		return nil
+	}
+
+	var x XMLPriceAvailResponse
+
+	err = xml.Unmarshal(response, &x)
+	if err != nil {
+		self.client.LogError(err)
+		return nil
+	}
+
+	// logger
+	self.client.LogData(skus[0], PA_URL, string(request), string(response))
+
+	return self.ToPriceAvailResult(&x)
+}
+
+func (self PriceAvail) BuildRequest(skus []string) []byte {
+	var x XMLPriceAvailSubmit
+
+	x.Header.Username = self.client.Username
+	x.Header.Password = self.client.Password
+	x.Header.Version = "1.4"
 
 	for _, sku := range skus {
 		sku = strings.TrimPrefix(sku, "TD-")
 		info := RequestLineInfo{"VP", sku}
-		in.Detail.LineInfo = append(in.Detail.LineInfo, info)
+		x.Detail.LineInfo = append(x.Detail.LineInfo, info)
 	}
 
 	// convert to xml
-	buf, _ := xml.MarshalIndent(in, "", "  ")
-	body := bytes.NewBuffer(buf)
+	body, _ := xml.MarshalIndent(x, "", "  ")
+	return body
+}
 
+func (self PriceAvail) SendRequest(url string, body []byte) ([]byte, error) {
 	// new http request
-	req, err := http.NewRequest("POST", PA_URL, body)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
-		c.LogError(err)
+		return nil, err
 	}
 
 	// http header
@@ -47,28 +76,19 @@ func (c *Client) GetPriceAvail(skus []string) *model.PriceAvailResult {
 	res, err := client.Do(req)
 	defer res.Body.Close()
 	if err != nil {
-		c.LogError(err)
+		return nil, err
 	}
 
 	// parse response
 	response, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		c.LogError(err)
+		return nil, err
 	}
 
-	var x XMLPriceAvailResponse
-	err = xml.Unmarshal(response, &x)
-	if err != nil {
-		c.LogError(err)
-	}
-
-	// logger
-	c.LogData(skus[0], PA_URL, string(buf), string(response))
-
-	return ToPriceAvailResult(&x)
+	return response, nil
 }
 
-func ToPriceAvailResult(x *XMLPriceAvailResponse) *model.PriceAvailResult {
+func (self PriceAvail) ToPriceAvailResult(x *XMLPriceAvailResponse) *model.PriceAvailResult {
 	r := &model.PriceAvailResult{}
 	return r
 }
