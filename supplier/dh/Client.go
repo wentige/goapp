cabb282e4c8348696536498c27f210e9fa9d2f53
Gopher L <zhuyf2000@gmail.com>
2019-10-30 08:47:52 -0400

    Client::SendRequest

--- supplier/dh/Client.go
+++ supplier/dh/Client.go
@@ -1,7 +1,12 @@
 package dh
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
+
 	"myapp/supplier/model"
 )
 
@@ -12,6 +17,9 @@ type Client struct {
 	Username string
 	Password string
 	//Logger
+
+	LastRequest  string
+	LastResponse string
 }
 
 func (c *Client) LogError(err error) {
@@ -33,4 +41,37 @@ func (c *Client) GetPriceAvail(skus []string) *model.PriceAvailResult {
 func (c *Client) Purchase(item map[string]string) *model.PurchaseResult {
 	po := &PurchaseOrder{c}
 	return po.Perform(item)
+}
+
+func (c *Client) SendRequest(method, url string, data []byte) ([]byte, error) {
+	// new http request
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	// http header
+	req.Header.Add("Content-Type", "application/xml; charset=utf-8")
+
+	// http client
+	client := &http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	// send request
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// read response
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	c.LastResponse = string(response)
+
+	return response, nil
 }
